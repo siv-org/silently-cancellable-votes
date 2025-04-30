f461a191b642a0ab1a80f830fac73fdf9550d934
David Ernst <git@dsernst.com>
2025-04-30 10:15:28 -0700

    prettier: ts/__tests__/cancelledVotes.test.ts

--- ts/__tests__/cancelledVotes.test.ts
+++ ts/__tests__/cancelledVotes.test.ts
@@ -1,27 +1,26 @@
-import { type WitnessTester } from "circomkit";
+import { type WitnessTester } from 'circomkit'
 
-import { circomkitInstance, getSignal } from "../utils.js";
-import { expect } from "chai";
+import { circomkitInstance, getSignal } from '../utils.js'
+import { expect } from 'chai'
 
-describe("Mulitplier circuit", function test() {
-  this.timeout(900000);
+describe('Mulitplier circuit', function test() {
+  this.timeout(900000)
 
-  let circuit: WitnessTester<["a", "b"], ["c"]>;
+  let circuit: WitnessTester<['a', 'b'], ['c']>
 
   before(async () => {
-    circuit = await circomkitInstance.WitnessTester("Multiplier", {
-      file: "./multiplier",
-      template: "Multiplier2",
+    circuit = await circomkitInstance.WitnessTester('Multiplier', {
+      file: './multiplier',
+      template: 'Multiplier2',
       params: [],
-    });
-  });
+    })
+  })
 
-  it("should multiply two numbers", async () => {
-    const a = BigInt(2);
-    const b = BigInt(3);
-    const c = await circuit.calculateWitness({ a, b });
-    const result = await getSignal(circuit, c, "c");
-    expect(result).to.equal(a * b);
-  });
-
-});
+  it('should multiply two numbers', async () => {
+    const a = BigInt(2)
+    const b = BigInt(3)
+    const c = await circuit.calculateWitness({ a, b })
+    const result = await getSignal(circuit, c, 'c')
+    expect(result).to.equal(a * b)
+  })
+})
