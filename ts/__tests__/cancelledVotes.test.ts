91eb4ebd783e919a92772258e76dee1b05607848
David Ernst <git@dsernst.com>
2025-05-04 12:58:10 -0700

    Remove unused ristretto_in_babyjubjub.circom file

--- ts/__tests__/cancelledVotes.test.ts
+++ ts/__tests__/cancelledVotes.test.ts
@@ -103,3 +103,19 @@ describe('Curve-25519 circuits', function test() {
     })
   })
 })
+
+/* More tests TODO:
+// Generate Random test cases:
+range(0,10).map(index => {
+    const point = generate_random_ristretto_point()
+    const scalar = generate_random_scalar()
+    const expected = point.multiply(scalar)
+
+    // Test cases that should work:
+    expect(test_scalar_multiplication(point, scalar, expected) === true)
+
+    // Test cases that should not work:
+    const different_scalar = generate_random_scalar()
+    expect(test_scalar_multiplication(point, different_scalar, expected) === false)
+})
+*/
